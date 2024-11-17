09c70cdd8d23b1e94332e0df805b0ed45ec27cc6
dadosha <55433356+dadosha@users.noreply.github.com>
2024-11-17 19:57:48 +0300

    Initial commit

---
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/MainActivityTest.java
@@ -0,0 +1,46 @@
+package ru.kkuzmichev.simpleappforespresso;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.qameta.allure.android.runners.AllureAndroidJUnit4;
+
+@LargeTest
+@RunWith(AllureAndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    String result = "This is home fragment";
+    String invalidResult = "This is home fragment111";
+
+    @Test
+    public void mainActivityTest() {
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction textView = onView(withId(R.id.text_home));
+        textView.check(matches(isDisplayed()));
+        textView.check(matches(withText(result)));
+    }
+}
